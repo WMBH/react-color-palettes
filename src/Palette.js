8c1f7c8f26a400be87759bc7fe4671a7ab6abd50
WMBH <itsyourwaytohell@gmail.com>
2019-08-15 13:23:57 +0300

    palettes component added

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+    render() {
+        return (
+            // nav here
+            <div className="Palette">
+                <div className="Palette-colors">{/* colors here */}</div>
+                {/* <footer></footer> */}
+            </div>
+        );
+    }
+}
