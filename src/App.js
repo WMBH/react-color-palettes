5f9a6d89114a3b4c1a2277916268d4d0b33741a8
WMBH <itsyourwaytohell@gmail.com>
2019-08-15 16:04:35 +0300

    chroma.js palette spread added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 function App() {
+    console.log(generatePalette(seedColors[2]));
     return (
         <div className="App">
             <Palette {...seedColors[2]} />
