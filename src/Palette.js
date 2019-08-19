9220c006319cbaf0bbe6af13cd828c9c674da739
WMBH <itsyourwaytohell@gmail.com>
2019-08-19 18:23:32 +0300

    Added footer

--- src/Palette.js
+++ src/Palette.js
@@ -19,12 +19,16 @@ export default class Palette extends Component {
 
     render() {
         const { level, format } = this.state;
-        const { colors } = this.props.palette;
+        const { colors, paletteName, emoji } = this.props.palette;
         const colorBoxes = colors[level].map((c) => <ColorBox background={c[format]} name={c.name} />);
         return (
             <div className="Palette">
                 <Navbar level={level} changeLevel={this.changeLevel} changeFormat={this.changeFormat} />
                 <div className="Palette-colors">{colorBoxes}</div>
+                <footer className="Palette-footer">
+                    {paletteName}
+                    <span className="emoji">{emoji}</span>
+                </footer>
             </div>
         );
     }
