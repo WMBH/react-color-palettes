ca69cda4556e96830569560a28417dae19eb6e9f
WMBH <itsyourwaytohell@gmail.com>
2019-08-19 18:03:39 +0300

    Finalized dropdown menu functionality

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
@@ -8,12 +11,16 @@ import './Navbar.css';
 class Navbar extends Component {
     constructor(props) {
         super(props);
-        this.state = { format: 'hex' };
+        this.state = { format: 'hex', open: false };
     }
 
-    handleChange = (e) => {
-        this.setState({ format: e.target.value });
+    handleFormatChange = (e) => {
+        this.setState({ format: e.target.value, open: true });
         this.props.changeFormat(e.target.value);
+    };
+
+    closeSnackbar = () => {
+        this.setState({ open: false });
     };
 
     render() {
@@ -32,12 +39,24 @@ class Navbar extends Component {
                     </div>
                 </div>
                 <div className="select-container">
-                    <Select value={format} onChange={this.handleChange}>
+                    <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
                         <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
                     </Select>
                 </div>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                    open={this.state.open}
+                    autoHideDuration={2000}
+                    message={<span id="message-id">Format changed to {format.toUpperCase()}</span>}
+                    action={[
+                        <IconButton onClick={this.closeSnackbar} color="inherit">
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                    onClose={this.closeSnackbar}
+                />
             </header>
         );
     }
