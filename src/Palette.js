8e48a3bee6272d72fd98043a478b68c9dc398d64
WMBH <itsyourwaytohell@gmail.com>
2019-08-20 20:37:22 +0300

    Refactor footer to separate component

--- src/Palette.js
+++ src/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 import './Palette.css';
 
 export default class Palette extends Component {
@@ -25,12 +26,14 @@ export default class Palette extends Component {
         ));
         return (
             <div className="Palette">
-                <Navbar level={level} changeLevel={this.changeLevel} changeFormat={this.changeFormat} />
+                <Navbar
+                    level={level}
+                    changeLevel={this.changeLevel}
+                    changeFormat={this.changeFormat}
+                    showSlider={true}
+                />
                 <div className="Palette-colors">{colorBoxes}</div>
-                <footer className="Palette-footer">
-                    {paletteName}
-                    <span className="emoji">{emoji}</span>
-                </footer>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         );
     }
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import ColorBox from './ColorBox';
+import PaletteFooter from './PaletteFooter';
 
 class SingleColorPalette extends Component {
     constructor(props) {
         super(props);
         this._shades = this.collectShades(this.props.palette, this.props.colorId);
+        this.state = {
+            format: 'hex'
+        };
     }
 
     collectShades = (palette, colorToFilterBy) => {
@@ -16,14 +21,21 @@ class SingleColorPalette extends Component {
         return shades.slice(1);
     };
 
+    changeFormat = (val) => {
+        this.setState({ format: val });
+    };
+
     render() {
+        const { format } = this.state;
+        const { paletteName, emoji } = this.props.palette;
         const colorBoxes = this._shades.map((c) => (
-            <ColorBox key={c.name} name={c.name} background={c.hex} showLink={false} />
+            <ColorBox key={c.name} name={c.name} background={c[format]} showLink={false} />
         ));
         return (
             <div className="Palette">
-                <h1>Adas</h1>
+                <Navbar changeFormat={this.changeFormat} showSlider={false} />
                 <div className="Palette-colors">{colorBoxes}</div>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         );
     }
