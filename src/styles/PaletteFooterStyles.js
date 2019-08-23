e2d6f88bcaba1b0edfc42b40eb76c452cea5ca50
WMBH <itsyourwaytohell@gmail.com>
2019-08-23 17:07:57 +0300

    Move style files in a separate folder

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,16 @@
+export default {
+    PaletteFooter: {
+        height: '3vh',
+        backgroundColor: 'white',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: '0.7rem',
+        flexBasis: 'auto'
+    },
+    emoji: {
+        fontSize: '1rem',
+        margin: '0 1rem'
+    }
+};
