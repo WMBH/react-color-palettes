de9acb21d70948b48cb2a095163ed75a23000560
WMBH <itsyourwaytohell@gmail.com>
2019-08-23 14:56:17 +0300

    Add dynamic text color via chroma.js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 
 export default class ColorBox extends Component {
     constructor(props) {
@@ -20,24 +21,26 @@ export default class ColorBox extends Component {
     render() {
         const { name, background, paletteId, id, showLink } = this.props;
         const { copied } = this.state;
+        const isDark = chroma(background).luminance() <= 0.08;
+        const isLight = chroma(background).luminance() >= 0.55;
         return (
             <div style={{ background }} className="ColorBox">
                 <div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
                 <div className={`copy-msg ${copied && 'show'}`}>
                     <h1>Copied!</h1>
-                    <p>{background}</p>
+                    <p className={isLight && 'dark-text'}>{background}</p>
                 </div>
                 <div className="copy-container">
                     <div className="box-content">
-                        <span>{name}</span>
+                        <span className={isDark && 'light-text'}>{name}</span>
                     </div>
                     <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-                        <button className="copy-button">Copy</button>
+                        <button className={`copy-button ${isLight && 'dark-text'}`}>Copy</button>
                     </CopyToClipboard>
                 </div>
                 {showLink && (
                     <Link to={`/palette/${paletteId}/${id}`}>
-                        <span className="see-more">More</span>
+                        <span className={`see-more ${isLight && 'dark-text'}`}>More</span>
                     </Link>
                 )}
             </div>
