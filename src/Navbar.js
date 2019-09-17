b3509ce58bfb40fcea6d02e49629dfcdbd93b5ea
WMBH <itsyourwaytohell@gmail.com>
2019-09-17 21:36:27 +0300

    Connect view of colors to color picker

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,59 +11,69 @@ import styles from './styles/NavbarStyles';
 import 'rc-slider/assets/index.css';
 
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { format: 'hex', open: false };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { format: 'hex', open: false };
+  }
 
-    handleFormatChange = (e) => {
-        this.setState({ format: e.target.value, open: true });
-        this.props.changeFormat(e.target.value);
-    };
+  handleFormatChange = e => {
+    this.setState({ format: e.target.value, open: true });
+    this.props.changeFormat(e.target.value);
+  };
 
-    closeSnackbar = () => {
-        this.setState({ open: false });
-    };
+  closeSnackbar = () => {
+    this.setState({ open: false });
+  };
 
-    render() {
-        const { level, changeLevel, showSlider, classes } = this.props;
-        const { format } = this.state;
+  render() {
+    const { level, changeLevel, showSlider, classes } = this.props;
+    const { format } = this.state;
 
-        return (
-            <header className={classes.Navbar}>
-                <div className={classes.logo}>
-                    <Link to="/">reactcolorpicker</Link>
-                </div>
-                {showSlider && (
-                    <div className="slider-container">
-                        <span>Level: {level}</span>
-                        <div className={classes.slider}>
-                            <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel} />
-                        </div>
-                    </div>
-                )}
-                <div className={classes.selectContainer}>
-                    <Select value={format} onChange={this.handleFormatChange}>
-                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
-                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
-                    </Select>
-                </div>
-                <Snackbar
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-                    open={this.state.open}
-                    autoHideDuration={2000}
-                    message={<span id="message-id">Format changed to {format.toUpperCase()}</span>}
-                    action={[
-                        <IconButton onClick={this.closeSnackbar} color="inherit">
-                            <CloseIcon />
-                        </IconButton>
-                    ]}
-                    onClose={this.closeSnackbar}
-                />
-            </header>
-        );
-    }
+    return (
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
+          <Link to="/">reactcolorpicker</Link>
+        </div>
+        {showSlider && (
+          <div className="slider-container">
+            <span>Level: {level}</span>
+            <div className={classes.slider}>
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+              />
+            </div>
+          </div>
+        )}
+        <div className={classes.selectContainer}>
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={this.state.open}
+          autoHideDuration={2000}
+          message={
+            <span id="message-id">
+              Format changed to {format.toUpperCase()}
+            </span>
+          }
+          action={[
+            <IconButton onClick={this.closeSnackbar} color="inherit">
+              <CloseIcon />
+            </IconButton>
+          ]}
+          onClose={this.closeSnackbar}
+        />
+      </header>
+    );
+  }
 }
 
 export default withStyles(styles)(Navbar);
