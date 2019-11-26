a93c69faa86d4c1c013a06298e4acdf96eb732e5
WMBH <itsyourwaytohell@gmail.com>
2019-11-26 14:01:17 +0300

    delete palettes dialog

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,9 +2,42 @@ import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import List from '@material-ui/core/List';
+import Avatar from '@material-ui/core/Avatar';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import styles from './styles/PaletteListStyles';
 
 class PaletteList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			deleteDialogOpen: false,
+			deleteId: ''
+		};
+	}
+
+	handleDelete = () => {
+		this.props.deletePalette(this.state.deleteId);
+		this.closeDeleteDialog();
+	};
+
+	openDeleteDialog = (id) => {
+		this.setState({ deleteDialogOpen: true, deleteId: id });
+	};
+
+	closeDeleteDialog = () => {
+		this.setState({ deleteDialogOpen: false, deleteId: '' });
+	};
+
 	redirectToPalette = (id) => {
 		this.props.history.push(`/palette/${id}`);
 	};
@@ -23,13 +56,35 @@ class PaletteList extends Component {
 							<MiniPalette
 								{...p}
 								handleRedirect={() => this.redirectToPalette(p.id)}
-								handleDelete={deletePalette}
+								// handleDelete={deletePalette}
+								openDialog={this.openDeleteDialog}
 								key={p.id}
 								id={p.id}
 							/>
 						))}
 					</div>
 				</div>
+				<Dialog open={this.state.deleteDialogOpen} onClose={this.closeDeleteDialog}>
+					<DialogTitle>Delete this palette?</DialogTitle>
+					<List>
+						<ListItem button onClick={this.handleDelete}>
+							<ListItemAvatar>
+								<Avatar>
+									<CheckIcon />
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText>Delete</ListItemText>
+						</ListItem>
+						<ListItem button onClick={this.closeDeleteDialog}>
+							<ListItemAvatar>
+								<Avatar>
+									<CloseIcon />
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText>Cancel</ListItemText>
+						</ListItem>
+					</List>
+				</Dialog>
 			</div>
 		);
 	}
