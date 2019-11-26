7e0af620ff2a13a72143de03aba359ccc3b4be3b
WMBH <itsyourwaytohell@gmail.com>
2019-11-26 15:33:12 +0300

    Updates

---
+++ precache-manifest.8a0e664d377e18d3c89afac878e3a66b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3478fd54ecbcde2703c0307d93e8b8",
+    "url": "/react-color-palettes/index.html"
+  },
+  {
+    "revision": "ec1d60675d27f4ed372b",
+    "url": "/react-color-palettes/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "a329e46c1f8f8cd0ffb1",
+    "url": "/react-color-palettes/static/css/main.215075d4.chunk.css"
+  },
+  {
+    "revision": "ec1d60675d27f4ed372b",
+    "url": "/react-color-palettes/static/js/2.48e0c37e.chunk.js"
+  },
+  {
+    "revision": "a329e46c1f8f8cd0ffb1",
+    "url": "/react-color-palettes/static/js/main.b4ae5dbe.chunk.js"
+  },
+  {
+    "revision": "55959ac5ca33c13844a0",
+    "url": "/react-color-palettes/static/js/runtime~main.5bdb1e70.js"
+  },
+  {
+    "revision": "0125c71fa858f731516940eebfe7d645",
+    "url": "/react-color-palettes/static/media/bg.0125c71f.svg"
+  }
+]);
